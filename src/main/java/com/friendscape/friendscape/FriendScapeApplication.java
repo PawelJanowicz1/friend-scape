5ae0e0c2b516308ed41adc74cb0b03bcbce06c82
Paweł Janowicz <janowiczpawel4@gmail.com>
2023-11-23 16:36:57 +0100

    adding standard files

---
+++ src/main/java/com/friendscape/friendscape/FriendScapeApplication.java
@@ -0,0 +1,13 @@
+package com.friendscape.friendscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendScapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendScapeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/friendscape/friendscape/FriendScapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friendscape.friendscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendScapeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
